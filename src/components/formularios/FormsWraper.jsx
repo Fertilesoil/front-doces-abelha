5127352245880cbb7c1cfb631642d64ceaa6d3ae
Fertilesoil <gasnbloop@gmail.com>
2024-04-16 17:51:07 -0300

    Teste de resolução do bug das imagens: esperado que elas carreguem

--- src/components/formularios/FormsWraper.jsx
+++ src/components/formularios/FormsWraper.jsx
@@ -1,13 +1,23 @@
-﻿import { formsWraperPropType } from "../../PropTypes/PropTypeValidation"
+﻿import { useEffect } from "react";
+import { formsWraperPropType } from "../../PropTypes/PropTypeValidation"
 
 
 const FormsWraper = ({ children, flex, borderLeft, borderRight }) => {
+
+  
+  useEffect(() => {
+    const aside = document.querySelector(".imagem");
+    aside.style.backgroundImage = "url('assets/images/abelhas-login.jpg')";
+
+    console.log(aside);
+  });
+
   return (
     <main className={`h-[100vh] flex justify-center items-center bg-gradient-to-l from-[#1D4151] to-[#8BBBC9]`}>
 
       <section className={`h-[80vh] w-[70vw] flex ${flex} justify-center items-center`}>
 
-        <aside className={`imagem w-[50%] h-full ${borderLeft} bg-login-image bg-center bg-cover bg-no-repeat`}>
+        <aside className={`imagem w-[50%] h-full ${borderLeft} bg-center bg-cover bg-no-repeat`}>
         </aside>
 
         <aside className={`w-[50%] h-full flex justify-center items-center bg-[#FCF5ED] ${borderRight}`}>
